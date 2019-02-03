095e18946c8ef1d2327810c72ecabac9e2500d78
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-02-03 11:26:26 -0500

    finished php app for fighter jets

---
+++ php/play.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PLAY</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+    <?php 
+      echo 'IT"S A TRAP!!!';
+
+    ?>
+
+  </body>
+</html>
